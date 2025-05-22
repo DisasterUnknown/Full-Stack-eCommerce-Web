c016687af364605b7c4330e99504c304fa562b43
DisasterUnknown <priyashanthafdo26@gmail.com>
2025-05-22 19:43:36 +0530

    Changed the styling in the admin view users page

--- JavaScript/viewUsers.js
+++ JavaScript/viewUsers.js
@@ -24,17 +24,13 @@ function FillThePageContents(data) {
     for (let i = 0; i < data['msg'].length; i++) {
         if (data['msg'][i]['UserRole'] !== "Admin") {
             pageDataElement.innerHTML += `
-                <div class="flex flex-col md:flex-row items-center justify-between py-5 mb-5 md:mb-1 bg-blue-600/10 w-90% xl:w-[80%] rounded-xl hover:scale-105 hover:shadow-[0_0_15px_2px_rgba(255,255,255,0.8)] transition-transform duration-300">
+                <div class="flex flex-col items-center justify-between py-5 mb-5 mx-1 md:mb-10 md:mx-4 bg-blue-600/10 w-[140px] md:w-[280px] rounded-xl hover:scale-105 hover:shadow-[0_0_15px_2px_rgba(255,255,255,0.8)] transition-transform duration-300">
                     <p id="userIdContainer${i}" class="hidden">${data['msg'][i]['UserID']}</p>
-                    <div class="flex flex-row items-center justify-between px-10 lx:px-5 md:pl-10 py-1">
-                        <img src=${data['msg'][i]['PFPdata'] == "null" ? "/WebProject/assets/uploadImg.webp" : data['msg'][i]['PFPdata']} alt="PFP" class="aspect-square w-[13%] mr-1 md:w-[5%] rounded-full">
-                        <p class="text-left md:w-40 font-semibold">${data['msg'][i]['Name']}</p>
-                        <p class="text-left md:w-40 font-semibold hidden md:block">${data['msg'][i]['Email']}</p>
-                        <p class="text-left md:w-40 font-semibold">${data['msg'][i]['UserRole']}</p>
-                    </div>
-                    <div class="flex mt-5 md:mt-0 mx-5 w-[100%] md:w-[20%] xl:w-[10%] justify-center">
-                        <button id="kickUserBtn${i}" class="border w-[30%] md:w-[100%] xl:px-5 py-1 rounded-xl hover:bg-white/5 hover:scale-105 hover:shadow-[0_0_15px_2px_rgba(0,0,255,0.8)] transition-transform duration-300">Kick</button>
-                    </div>
+                        <img src=${data['msg'][i]['PFPdata'] == "null" ? "/WebProject/assets/uploadImg.webp" : data['msg'][i]['PFPdata']} alt="PFP" class="aspect-square w-[64px] mr-1 rounded-full">
+                        <p class="text-center md:w-40 font-semibold">${data['msg'][i]['Name']}</p>
+                        <p class="text-center md:w-40 font-semibold">${data['msg'][i]['UserRole']}</p>
+                        <p class="text-center text-[clamp(0.8rem, 2.5vw, 1.2rem)] md:w-full font-semibold hidden md:block">${data['msg'][i]['Email']}</p>
+                        <button id="kickUserBtn${i}" class="border mt-5 w-[100px] md:w-[100px] xl:px-5 py-1 rounded-xl hover:bg-white/5 hover:scale-105 hover:shadow-[0_0_15px_2px_rgba(0,0,255,0.8)] transition-transform duration-300">Kick</button>
                 </div>`;
         }
     }
--- Pages/viewUsers.php
+++ Pages/viewUsers.php
@@ -6,20 +6,8 @@ $scriptPage = "/WebProject/JavaScript/viewUsers.js";
 $content = <<<HTML
     <div class="space-y-4 min-h-[calc(100vh-92px)]" id="viewUsersPage">
         <p class="text-2xl font-bold text-center mt-8 mb-10">View Users</p>
-        <div id="userSections" class="flex flex-row flex-wrap justify-evenly mt-8">
+        <div id="userSections" class="flex flex-row flex-wrap justify-center items-center mt-8 gap-4">
             <!-- Page Data -->
-            <!-- <div class="flex flex-col md:flex-row items-center justify-between py-5 mb-5 md:mb-1 bg-blue-600/10 w-90% xl:w-[80%] rounded-xl hover:scale-105 hover:shadow-[0_0_15px_2px_rgba(255,255,255,0.8)] transition-transform duration-300">
-                <p id="userIdContainer" class="hidden"></p>
-                <div class="flex flex-row items-center justify-between px-10 lx:px-5 md:pl-10 py-1">
-                    <img src="/WebProject/assets/uploadImg.webp" alt="PFP" class="aspect-square w-[13%] md:w-[5%] rounded-full">
-                    <p class="font-semibold">Name</p>
-                    <p class="font-semibold hidden md:block">Email</p>
-                    <p class="font-semibold">Role</p>
-                </div>
-                <div class="flex mt-5 md:mt-0 mx-5 w-[100%] md:w-[10%] justify-center">
-                    <button class="border w-[30%] md:w-[100%] xl:px-5 py-1 rounded-xl hover:bg-white/5 hover:scale-105 hover:shadow-[0_0_15px_2px_rgba(0,0,255,0.8)] transition-transform duration-300">Kick</button>
-                </div>
-            </div> -->
         </div>
     </div>
 
